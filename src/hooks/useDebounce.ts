d5b7a3b67a1da1553e2906d6b2a0f7fa380620d5
Brian Travis <bztravis88@gmail.com>
2024-02-02 18:46:20 -0500

    Add preferred useDebounce

--- src/hooks/useDebounce.ts
+++ src/hooks/useDebounce.ts
@@ -1,13 +1,33 @@
-import { useEffect, EffectCallback } from 'react';
-import useTimeout from './useTimeout';
+// import { useEffect, EffectCallback } from 'react';
+// import useTimeout from './useTimeout';
 
-export default function useDebounce(
-  callback: EffectCallback,
-  delay: number,
-  dependencies: unknown[]
-) {
-  const { reset, clear } = useTimeout(callback, delay);
+// export default function useDebounce(
+//   callback: EffectCallback,
+//   delay: number,
+//   dependencies: unknown[]
+// ) {
+//   const { reset, clear } = useTimeout(callback, delay);
 
-  useEffect(reset, [...dependencies, reset]);
-  useEffect(clear, [clear]); // don't run on first mount
-}
+//   useEffect(reset, [...dependencies, reset]);
+//   useEffect(clear, [clear]); // don't run on first mount
+// }
+
+import { useState, useEffect } from 'react';
+
+const useDebounce = <T>(value: T, delay = 500) => {
+  const [debouncedValue, setDebouncedValue] = useState<T>(value);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [value, delay]);
+
+  return { debouncedValue, setInstantly: setDebouncedValue };
+};
+
+export default useDebounce;
